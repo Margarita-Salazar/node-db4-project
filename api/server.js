18f2d971188cc8d1cf507329caafce5234b53fd4
unknown <margarita.salazar.mls@gmail.com>
2021-06-10 19:41:40 -0500

    settup for server

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('*', (req, res)=>{
+    res.send('<h1>Its working!!</h1>')
+})
+
+module.exports = server
